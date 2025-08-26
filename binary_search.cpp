c73019360c6c8493b05e7176aa6f96fa0f56d91f
Nikita Vasilev <ioganurij953@gmail.com>
2025-08-26 19:54:19 +0800

    huhuhu

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,35 +1,47 @@
 #include <iostream>
 #include <vector>
 
-bool BinarySearch(std::vector<int>& v, int target) {
+int BinarySearch(std::vector<int>& arr, int target) {
   int low = 0;
-  int high = v.size() - 1;
+  int high = arr.size() - 1;
+  int ans = -1;
 
   while (low <= high) {
-    int mid = ((high - low) / 2) + low;
-
-    if (v[mid] == target) {
-      return true;
-    }
-
-    if (v[mid] > target) {
-      high = mid - 1;
+    int middle = (high + low) / 2;
+    if (arr[middle] == target) {
+      ans = middle;
+      break;
+    } else if (arr[middle] < target) {
+      low = middle + 1;
     } else {
-      low = mid + 1;
+      high = middle - 1;
     }
   }
 
-  return false;
+  return ans;
 }
 
 int main(void) {
-  std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-  int target = 3;
+  std::vector<int> arr;
+  int n = 0;
 
-  if (BinarySearch(v, target)) {
-    std::cout << target << " found.";
+  std::cin >> n;
+  
+  while (n--) {
+    int i = 0;
+    std::cin >> i;
+    arr.push_back(i);
+  }
+
+  int target = 0;
+  std::cin >> target;
+
+  int found = BinarySearch(arr, target);
+
+  if (found == -1) {
+    std::cout << "Not found\n";
   } else {
-    std::cout << target << " not found.";
+    std::cout << target << " found at " << found << '\n';
   }
 
   return 0;
